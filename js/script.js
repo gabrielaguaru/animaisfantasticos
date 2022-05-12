bdf1d175fabf57e92c9797603e12d084e6cd0a18
Gabriela Souza <gabrielaguaru@yahoo.com.br>
2022-05-12 16:01:56 -0300

    mudar numeros para dinamico usando API animais-numeros e async await

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from "./modules/modal.js"
 import initTooltip from "./modules/tooltip.js"
 import initDropdown from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js"
-import initAnimaNumeros from "./modules/anima-numeros.js"
 import initFuncionamento from "./modules/funcionamento.js"
+import initFetchAnimais from "./modules/fetch-animais.js"
 
 initAnimationScroll()
 initSmoothScroll();
@@ -17,5 +17,5 @@ initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
