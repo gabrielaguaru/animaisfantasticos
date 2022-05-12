041ea5987e0ae989d122a315a6b749518a964d00
Gabriela Souza <gabrielaguaru@yahoo.com.br>
2022-05-12 16:12:48 -0300

    add try catch

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from "./modules/modal.js"
 import initTooltip from "./modules/tooltip.js"
 import initDropdown from "./modules/dropdown-menu.js"
 import initMenuMobile from "./modules/menu-mobile.js"
+
 import initFuncionamento from "./modules/funcionamento.js"
 import initFetchAnimais from "./modules/fetch-animais.js"
 
